b84bc8b9b6fd8c4ce7ff8c1ffd98ee2d0fdcb085
Ruslan <89604587433@mail.ru>
2022-10-12 22:40:03 +0300

    написана программа которая выделяет из массива элемент с длинной <= 3 символа

--- HomeTask/Program.cs
+++ HomeTask/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  метод считывает с консоли число тип int положительное и >0
+int ReadNamberInt()
+{
+    int a=0;
+    bool parsNIsOk=false;
+    while (!parsNIsOk)
+    {
+        
+        parsNIsOk=int.TryParse(Console.ReadLine(), out int numberN);
+        if ((!parsNIsOk)||(numberN<=0))
+        {
+            Console.WriteLine("Введено неверно, введите заново:");
+            parsNIsOk=false;
+        }
+        else a=numberN;
+    }
+    return a;
+}
+
+// метод выделяет из массива заданную длину строк 
+
+void ArrangeArray(string[] array)
+{
+    int n=array.Length;
+    int strLength=0;
+    for(int j=0; j<n; j++)
+    {
+      strLength=array[j].Length;
+      if (strLength<=3)
+        {
+            Console.WriteLine(array[j]);
+        }
+    }
+
+
+}
+
+int n=0;
+Console.Clear();
+Console.Write("Введите размерность массива :");
+ n=ReadNamberInt();
+string[] array=new string[n];
+for(int j=0; j<n; j++)
+    {
+        Console.Write($"Введите {j} элемент массива :");
+        array[j]=Console.ReadLine();
+        // Console.Write($" {j} = [{ array[j] }] ");
+    }
+    ArrangeArray(array);
